1ecaadc6736d53466ae4ce8836805049829520bb
KristStudio <zhikangliu@hotmail.com>
2021-01-31 16:18:09 +0800

    Update TextBlock.java

--- core/src/com/team23/game/ui/controls/labels/TextBlock.java
+++ core/src/com/team23/game/ui/controls/labels/TextBlock.java
@@ -3,16 +3,15 @@ package com.team23.game.ui.controls.labels;
 import com.team23.game.ui.UIElement;
 
 public class TextBlock extends UIElement {
-    Label label
     public TextBlock(){
 
     }
 
-    @Override
     public void act(){
 
     }
 
-    @Override
-    public 
+    public void draw(){
+
+    }
 }
