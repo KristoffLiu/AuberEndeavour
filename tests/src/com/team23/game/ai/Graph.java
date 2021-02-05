76216e3d73e7872dcc09db57b057e16af798d86a
mrk531 <72560774+mrk531@users.noreply.github.com>
2021-02-05 05:39:32 +0000

    Added more tests

--- tests/src/com/team23/game/ai/Graph.java
+++ tests/src/com/team23/game/ai/Graph.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
+import javax.xml.validation.Validator;
 import java.util.ArrayList;
 
 public class Graph {
@@ -60,5 +61,84 @@ public class Graph {
 
         // Get nearest node when on the node
         Assert.assertEquals(graph.getNearestNode(new Vector2(26,3)), nodes.get(0));
+
+        // Check when there are no nodes
+        PathGraph tempGraph = new PathGraph();
+        Assert.assertEquals(tempGraph.getNearestNode(new Vector2(2,2)).position, new Vector2(2147483647, 2147483647));
+    }
+
+    @Test
+    public void generatePath(){
+
+        // Basic path
+        Assert.assertEquals(graph.findPath(nodes.get(4), nodes.get(10)), nodes.get(0));
+
+        // No path
+        PathGraph tempGraph = new PathGraph();
+        PathNode node1 = new PathNode(new Vector2(2,2), false);
+        PathNode node2 = new PathNode(new Vector2(4,2), false);
+        PathNode node3 = new PathNode(new Vector2(2,4), false);
+        PathNode node4 = new PathNode(new Vector2(4,4), false);
+
+        tempGraph.addNode(node1);
+        tempGraph.addNode(node2);
+        tempGraph.addNode(node3);
+        tempGraph.addNode(node4);
+
+        Assert.assertNull(tempGraph.findPath(node1, node4));
+
+        // 2 Paths
+
+        tempGraph.addEdge(node1,node2);
+        tempGraph.addEdge(node1,node3);
+        tempGraph.addEdge(node3,node4);
+        tempGraph.addEdge(node2,node4);
+
+        PathNode result = tempGraph.findPath(node1,node4);
+
+        Assert.assertTrue(result == node2 || result == node3);
+
+
+    }
+
+    @Test
+    public void getNodeWithMostEdges(){
+        // node with only 1 edge
+        Assert.assertEquals(graph.getMostEdgesAdjacentNode(nodes.get(5)), nodes.get(2));
+
+        // node where you have to work it out
+        Assert.assertEquals(graph.getMostEdgesAdjacentNode(nodes.get(9)), nodes.get(2));
+
+        // nodes have both the same number of edges
+        PathNode node1 = new PathNode(new Vector2(2,2), false);
+        PathNode node2 = new PathNode(new Vector2(3,3), false);
+        PathNode node3 = new PathNode(new Vector2(4,4), false);
+
+        // Tests adding nodes as an array
+        PathNode[] tempNodes = {node1};
+        PathGraph tempGraph = new PathGraph(tempNodes);
+
+        // Testing when there is only 1 node
+        PathNode result = tempGraph.getMostEdgesAdjacentNode(node1);
+        Assert.assertEquals(result.position, new Vector2(-1, -1));
+
+        // Test when the nodes have the same number of edges
+        tempGraph.addNode(node2);
+        tempGraph.addNode(node3);
+        tempGraph.addEdge(node1, node2);
+        tempGraph.addEdge(node2, node3);
+
+        result = tempGraph.getMostEdgesAdjacentNode(node2);
+        Assert.assertTrue((result == node1 || result == node3));
+
+    }
+
+
+    @Test
+    public void getRandomNodes(){
+        for (int i = 0; i < 10; i++) {
+            Assert.assertTrue(graph.getRandomWorkingSystem().isWorkingSystem());
+            Assert.assertTrue(graph.getRandomNonSystem().isNonSystem());
+        }
     }
 }
