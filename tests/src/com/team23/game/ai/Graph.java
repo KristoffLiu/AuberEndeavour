be86ec7df04ec11d6a33a421ee194b1b6f273c95
mrk531 <72560774+mrk531@users.noreply.github.com>
2021-02-09 09:21:47 +0000

    Added more tests

--- tests/src/com/team23/game/ai/Graph.java
+++ tests/src/com/team23/game/ai/Graph.java
@@ -51,17 +51,26 @@ public class Graph {
     }
 
     @Test
-    public void nearestNode(){
+    public void nearestNode() {
         // Get nearest node (21,9)
-        Assert.assertEquals(graph.getNearestNode(new Vector2(20,10)), nodes.get(9));
+        Assert.assertEquals(graph.getNearestNode(new Vector2(20, 10)), nodes.get(9));
+    }
 
+    @Test
+    public void nearestNodeEqual() {
         // Get nearest node when in the middle of two
-        Assert.assertTrue((graph.getNearestNode(new Vector2(28,3)) == nodes.get(0) ||
-                graph.getNearestNode(new Vector2(28,3)) == nodes.get(4) ));
-
+        Assert.assertTrue((graph.getNearestNode(new Vector2(28, 3)) == nodes.get(0) ||
+                graph.getNearestNode(new Vector2(28, 3)) == nodes.get(4)));
+    }
+    @Test
+    public void nearestNodeOnNode() {
         // Get nearest node when on the node
-        Assert.assertEquals(graph.getNearestNode(new Vector2(26,3)), nodes.get(0));
+        Assert.assertEquals(graph.getNearestNode(new Vector2(26, 3)), nodes.get(0));
 
+    }
+
+    @Test
+    public void nearestNodeNoNode() {
         // Check when there are no nodes
         PathGraph tempGraph = new PathGraph();
         Assert.assertEquals(tempGraph.getNearestNode(new Vector2(2,2)).position, new Vector2(2147483647, 2147483647));
@@ -98,41 +107,62 @@ public class Graph {
 
         Assert.assertTrue(result == node2 || result == node3);
 
+    }
+
+    @Test
+    public void getNodeWithMostEdgesSimple() {
+        // node with only 1 edge
+        Assert.assertEquals(graph.getMostEdgesAdjacentNode(nodes.get(5)), nodes.get(2));
+    }
+
+    @Test
+    public void getNodeWithMostEdges() {
+        // node where you have to work it out
+        Assert.assertEquals(graph.getMostEdgesAdjacentNode(nodes.get(9)), nodes.get(2));
 
     }
 
     @Test
-    public void getNodeWithMostEdges(){
-        // node with only 1 edge
-        Assert.assertEquals(graph.getMostEdgesAdjacentNode(nodes.get(5)), nodes.get(2));
-
-        // node where you have to work it out
-        Assert.assertEquals(graph.getMostEdgesAdjacentNode(nodes.get(9)), nodes.get(2));
-
+    public void getNodeWithMostEdgesEqual() {
         // nodes have both the same number of edges
-        PathNode node1 = new PathNode(new Vector2(2,2), false);
-        PathNode node2 = new PathNode(new Vector2(3,3), false);
-        PathNode node3 = new PathNode(new Vector2(4,4), false);
-
+        PathNode node1 = new PathNode(new Vector2(2, 2), false);
+        PathNode node2 = new PathNode(new Vector2(3, 3), false);
+        PathNode node3 = new PathNode(new Vector2(4, 4), false);
+    }
+    @Test
+    public void addingNodesWithArray() {
         // Tests adding nodes as an array
+        PathNode node1 = new PathNode(new Vector2(2, 2), false);
+        PathNode[] tempNodes = {node1};
+        PathGraph tempGraph = new PathGraph(tempNodes);
+    }
+
+    @Test
+    public void getNodeWithMostEdges1Node() {
+        PathNode node1 = new PathNode(new Vector2(2, 2), false);
         PathNode[] tempNodes = {node1};
         PathGraph tempGraph = new PathGraph(tempNodes);
 
         // Testing when there is only 1 node
         PathNode result = tempGraph.getMostEdgesAdjacentNode(node1);
         Assert.assertEquals(result.position, new Vector2(-1, -1));
+    }
 
+    @Test
+    public void getNodeWithMostEdgesSameEdges() {
         // Test when the nodes have the same number of edges
-        tempGraph.addNode(node2);
-        tempGraph.addNode(node3);
+        PathNode node1 = new PathNode(new Vector2(2, 2), false);
+        PathNode node2 = new PathNode(new Vector2(3, 3), false);
+        PathNode node3 = new PathNode(new Vector2(4, 4), false);
+        PathNode[] tempNodes = {node1,node2,node3};
+        PathGraph tempGraph = new PathGraph(tempNodes);
         tempGraph.addEdge(node1, node2);
         tempGraph.addEdge(node2, node3);
 
-        result = tempGraph.getMostEdgesAdjacentNode(node2);
+        PathNode result = tempGraph.getMostEdgesAdjacentNode(node2);
         Assert.assertTrue((result == node1 || result == node3));
 
     }
-
 
     @Test
     public void getRandomNodes(){
--- tests/src/com/team23/game/ai/Queue.java
+++ tests/src/com/team23/game/ai/Queue.java
@@ -6,22 +6,27 @@ import com.team23.game.ai.graph.queue.PriorityItem;
 import com.team23.game.ai.graph.queue.PriorityQueue;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.*;
 
 public class Queue {
 
     @Test
     public void testPop() {
         PriorityQueue testQueue = new PriorityQueue();
+
         PathNode node1 = new PathNode(new Vector2(2,2), false);
         PathNode node2 = new PathNode(new Vector2(2,2), false);
+
         PriorityItem item = new PriorityItem(node1,node2,9,1);
+
         testQueue.push(item);
+
         PriorityItem result = testQueue.pop();
         Assert.assertEquals(result, item);
     }
 
     @Test
-    public void testPrioritypop(){
+    public void testPriorityPop(){
         PriorityQueue testQueue = new PriorityQueue();
 
         PathNode node1 = new PathNode(new Vector2(2,2), false);
@@ -41,5 +46,29 @@ public class Queue {
         Assert.assertEquals(result, priority2);
         result = testQueue.pop();
         Assert.assertEquals(result, priority3);
+    }
+
+
+    @Test
+    public void samePriority(){
+        PathNode node1 = new PathNode(new Vector2(2,2), false);
+        PathNode node2 = new PathNode(new Vector2(2,2), false);
+
+        PriorityQueue testQueue = new PriorityQueue();
+        PriorityItem priority2 = new PriorityItem(node1,node2,2,1);
+        PriorityItem priority1 = new PriorityItem(node1,node2,2,1);
+        testQueue.push(priority1);
+        testQueue.push(priority2);
+
+        PriorityItem result = testQueue.pop();
+        Assert.assertTrue((result.equals(priority1) || result.equals(priority2)));
+
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testEmptyPop(){
+        PriorityQueue testQueue = new PriorityQueue();
+        testQueue.pop();
+
     }
 }
